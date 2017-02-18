c16d72c4fe420fe80572046ae046f20de07b4c77
Brooks Swinnerton <bswinnerton@gmail.com>
2017-02-18 12:19:46 -0500

    Add simple login / repo browser opener

--- Contents/Scripts/default.js
+++ Contents/Scripts/default.js
@@ -1,11 +1,9 @@
 // LaunchBar Action Script
 
 function run(argument) {
-    if (argument == undefined) {
-        // Inform the user that there was no argument
-        LaunchBar.alert('No argument was passed to the action');
-    } else {
-        // Return a single item that describes the argument
-        return [{ title: '1 argument passed'}, { title : argument }];
-    }
+  runWithString(argument);
+}
+
+function runWithString(string) {
+  LaunchBar.openURL('https://github.com/' + string);
 }

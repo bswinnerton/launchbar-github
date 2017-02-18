d20ec1aef79d61df1de3d3500e4c9a38f16bde42
Brooks Swinnerton <bswinnerton@gmail.com>
2017-02-18 12:53:42 -0500

    Add support for rails/rails#123 formatted strings

--- Contents/Scripts/default.js
+++ Contents/Scripts/default.js
@@ -5,5 +5,24 @@ function run(argument) {
 }
 
 function runWithString(string) {
-  LaunchBar.openURL('https://github.com/' + string);
+  // Matching:
+  // rails/rails#123
+  // rails/rails/issues/123
+  // rails/rails/pull/123
+  if (match = string.match(/^([^\/]+\/[^\/#]+)(?:\/pull\/|\/issues\/|#)(\d+)$/)) {
+    var nwo     = match[1],
+        number  = match[2];
+
+    openIssue(nwo, number)
+  } else {
+    openUser(string);
+  }
+}
+
+function openIssue(nameWithOwner, number) {
+  LaunchBar.openURL('https://github.com/' + nameWithOwner + '/issues/' + number);
+}
+
+function openUser(user) {
+  LaunchBar.openURL('https://github.com/' + user);
 }

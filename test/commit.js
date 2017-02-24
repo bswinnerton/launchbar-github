93c648b9a0517ce0eaf9c41908ebaca8b6c33a16
Brooks Swinnerton <bswinnerton@gmail.com>
2017-02-23 23:25:37 -0500

    Add test for Commit#searchURL

--- test/commit.js
+++ test/commit.js
@@ -6,6 +6,12 @@ describe('commit', function() {
   let sha = 'e6557a15';
   let commit = new Commit(sha);
 
+  describe('#searchURL', function() {
+    it('returns the URL that can be used to search for the commit', function() {
+      assert.equal('https://github.com/search?q=e6557a15&type=Commits&utf8=%E2%9C%93', commit.searchURL);
+    });
+  });
+
   describe('#pullRequests()', function() {
     it('returns the pull requests where the commit was introduced');
   });

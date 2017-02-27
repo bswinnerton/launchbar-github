7c333902f6acb359555b5903d431c6175746b344
Brooks Swinnerton <bswinnerton@gmail.com>
2017-02-26 20:33:56 -0500

    Don't open browser when no token is set

--- Contents/Scripts/graphql.js
+++ Contents/Scripts/graphql.js
@@ -2,12 +2,10 @@ let GraphQL = {};
 
 GraphQL.execute = function(query, variables) {
   if (!Action.preferences.token) {
-    LaunchBar.openURL('https://github.com/settings/tokens');
     LaunchBar.alert("It looks like this is the first time you're using " +
       "this action.\n\nPlease go to https://github.com/settings/tokens " +
       "and create token with 'repo' and 'user' scope and set it by invoking " +
       "the github action and going to settings.");
-    return;
   }
 
   let requestHeaders  = { authorization: 'token ' + Action.preferences.token };

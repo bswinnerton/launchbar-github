bswinnerton/launchbar-github

commit 42ef12814905242ac7a321c4b447373832f20a69

Update regexes to match non word characters
So that we can match accounts and repositories with hyphens

Contents/Scripts/default.js
@@ -66,10 +66,10 @@ class GitHubLB {
 
   displayMenuItemFor(input) {
     const GITHUB_LINK_FORMAT  = /^https?:\/\/((www|gist|raw)\.)?github\.(io|com)/;
-    const ISSUE_OR_PR_FORMAT  = /^(\w+)\/(\w+)#(\d+)?\s*(\w+)?$/;
-    const REPOSITORY_FORMAT   = /^(\w+)\/(.*)?\s*(\w+)?$/;
+    const ISSUE_OR_PR_FORMAT  = /^(.*)\/(.*)#(\d+)?\s*(.*)?$/;
+    const REPOSITORY_FORMAT   = /^(.*)\/(.*)?\s*(.*)?$/;
     const COMMIT_SHA_FORMAT   = /^\b[0-9a-f]{5,40}\b$/;
-    const ACCOUNT_FORMAT      = /^(\w+)?\s*(\w+)?$/;
+    const ACCOUNT_FORMAT      = /^(.*)?\s*(.*)?$/;
 
     let match;
 

commit 4c99d1ce395ee29342d6d6932b80a89c62f57f18

Update regex to support secondary search
Early on in the development of GitHub for LaunchBar, the action
supported "secondary searches" where if you added a space after your
primary search, you could filter down the menu even more. For example,
with something like:

`bswinnerton issues`

You would see only the issues menu item available. This appears to have
broken in 42ef128, and unfortunately no tests caught this.

This commit resolves the problem by reverting back to word-based regex
support.

Contents/Scripts/default.js
@@ -74,10 +74,10 @@ class GitHubLB {
 
   displayMenuItemFor(input) {
     const GITHUB_LINK_FORMAT  = /^https?:\/\/((www|gist|raw|developer)\.)?github\.(io|com)/;
-    const ISSUE_OR_PR_FORMAT  = /^(.*)\/(.*)#(\d+)?\s*(.*)?$/;
-    const REPOSITORY_FORMAT   = /^(.*)\/(.*)?\s*(.*)?$/;
+    const ISSUE_OR_PR_FORMAT  = /^(\w+)\/(\w+)#(\d+)?\s*(\w+)?$/;
+    const REPOSITORY_FORMAT   = /^(\w+)\/(\w+)?\s*(\w+)?$/;
     const COMMIT_SHA_FORMAT   = /^\b[0-9a-f]{5,40}\b$/;
-    const ACCOUNT_FORMAT      = /^(.*)?\s*(.*)?$/;
+    const ACCOUNT_FORMAT      = /^(\w+)\s*(\w+)?$/;
 
     let match;
 
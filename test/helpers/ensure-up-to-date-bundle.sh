cf0e1e101096c282a8765223198a9cbe35ee1481
Brooks Swinnerton <bswinnerton@gmail.com>
2017-10-29 13:16:28 -0400

    Add test to ensure script/build was run
    When submitting code to this project, it's important to ensure that
    `Contents/Scripts/bundle.min.js` was updated since that's the compiled
    file that Launchbar uses to execute this Action.
    
    This commit adds a test to ensure that the file was updated.

---
+++ test/helpers/ensure-up-to-date-bundle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+exitcode=0
+
+cp Contents/Scripts/bundle.min.js Contents/Scripts/bundle.min.js.tmp
+
+script/build
+
+if ! cmp -s Contents/Scripts/bundle.min.js Contents/Scripts/bundle.min.js.tmp; then
+  echo "The source of launchbar-github has changed but the corresponding bundle.min.js file has not."
+  echo
+  echo "Please run \`script/build\` and commit the resulting changes."
+  exitcode=1
+fi
+
+rm Contents/Scripts/bundle.min.js.tmp
+
+exit $exitcode

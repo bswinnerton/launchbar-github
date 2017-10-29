9c47f593798c21d1256ff1c8e6fcf662776b6662
Brooks Swinnerton <bswinnerton@gmail.com>
2017-10-29 14:09:25 -0400

    Show Babel version in error message

--- test/helpers/ensure-up-to-date-bundle.sh
+++ test/helpers/ensure-up-to-date-bundle.sh
@@ -7,9 +7,11 @@ cp Contents/Scripts/bundle.min.js Contents/Scripts/bundle.min.js.tmp
 script/build
 
 if ! cmp -s Contents/Scripts/bundle.min.js Contents/Scripts/bundle.min.js.tmp; then
+  babel_version="$(./node_modules/.bin/babel --version)"
+
   echo "The source of launchbar-github has changed but the corresponding bundle.min.js file has not."
   echo
-  echo "Please run \`script/build\` and commit the resulting changes."
+  echo "Please ensure you are running Babel version $babel_version, and then run \`script/build\` and commit the resulting changes."
 
   if [ -n "$DEBUG" ]; then
     git --no-pager diff Contents/Scripts/bundle.min.js Contents/Scripts/bundle.min.js.tmp
